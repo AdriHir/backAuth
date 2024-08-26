5b08898c849421731ee514b70c78469f70bfa5c7
AdriHir <hirtzadrien@gmail.com>
2024-08-26 13:17:54 +0200

    BackAuth

---
+++ src/Entity/User.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements UserInterface,PasswordAuthenticatedUserInterface{
+
+
+    public function __construct(
+        private ?string $role = null,
+        private ?string $email = null,
+        private ?string $mdp =null,
+        private ?int $id = null
+    ) {}
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function setId(?int $id): self {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getEmail(): ?string {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self{
+        $this->email = $email;
+        return $this;
+    }
+    public function getMdp(): ?string {
+        return $this->mdp
+        ;
+    }
+
+    public function setMdp(?string $mdp): self{
+        $this->mdp = $mdp;
+        return $this;
+    }
+    public function getRole(): ?string {
+        return $this->role;
+    }
+
+    public function setRole(?string $role): self{
+        $this->role = $role;
+        return $this;
+    }
+    public function getRoles(): array{
+        return [$this->role];
+    }
+    public function eraseCredentials(): void{}
+    public function getUserIdentifier(): string{
+        return $this->email;
+    }
+    public function getPassword(): string|null{
+        return $this->mdp;
+    }
+}
---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repository;
+use App\Entity\User;
+
+class UserRepository {
+
+
+    public function persist(User $user) {
+        $connection = database::connect();
+        $query = $connection->prepare('INSERT INTO user (email,mdp,role) VALUES(:email,:mdp,:role)');
+        $query->bindValue(':email', $user->getEmail());
+        $query->bindValue(':mdp', $user->getMdp());
+        $query->bindValue(':role', $user->getRole());
+        $query->execute();
+        $user->setId($connection->lastInsertId());
+    }
+    public function findByEmail(string $email): ?User {
+        $connection = Database::connect();
+        $query = $connection->prepare('SELECT * FROM user WHERE email=:email');
+        $query->bindValue(':email', $email);
+        $query->execute();
+
+        if($line = $query->fetch()) {
+            $user = new User();
+            $user->setRole($line['role']);
+            $user->setEmail($line['email']);
+            $user->setMdp($line['mdp']);
+            $user->setId($line['userId']);
+            return $user;
+        }
+        return null;
+    }
+}
