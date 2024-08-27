945df1af290577ea9140f4cc753e0fad2b5a2347
AdriHir <hirtzadrien@gmail.com>
2024-08-27 09:55:16 +0200

    backAuthfinish

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 use App\Entity\User;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
